f6294658a5f35da400a091f4a79cfa62103df950
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-06-10 05:56:11 +0600

    jwt issue solved

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
 const jwt = require('jsonwebtoken');
+require('dotenv').config()
 const  port = process.env.PORT || 5000;
  
 //  middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req,res,next)=>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message: 'unauthorized access'})
+  }
+
+  const token = authorization.split(' ')[1];
+
+
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 // get data at server home
 app.get('/', (req,res)=>{
@@ -36,7 +54,15 @@ async function run() {
 
     // all About Jwt
 
-    
+    app.post('/jwt',(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+
+      res.send({ token })
+
+    })
+
+
 
 // database collections
 const allDataCollection = client.db("summerCamp").collection("allData");
