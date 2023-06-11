41ec7388df8581e138572998f761be530a2d8480
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-06-12 01:19:20 +0600

    admin role created

--- index.js
+++ index.js
@@ -88,6 +88,34 @@ app.post('/users', async(req,res)=>{
   res.send(result)
 })
 
+// admin and instructor making
+app.patch('/users/admin/:id', async (req,res)=>
+{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+   const updateDoc = {
+    $set: {
+      role: 'admin'
+    },
+   };
+   const result = await usersCollection.updateOne(filter,updateDoc);
+   res.send(result);
+})
+
+// Instructor
+app.patch('/users/Instructor/:id', async (req,res)=>
+{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)};
+   const updateDoc = {
+    $set: {
+      role: 'Instructor'
+    },
+   };
+   const result = await usersCollection.updateOne(filter,updateDoc);
+   res.send(result);
+})
+
 app.get('/allData', async (req,res)=>{
 
     const result = await allDataCollection.find().toArray();
