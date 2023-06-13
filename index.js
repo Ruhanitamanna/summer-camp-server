e80c0438904adff8b88dc04069b15b34af96c36d
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-06-14 01:35:55 +0600

    updated

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const stripe = require('stripe')
-require('dotenv').config()(process.env.PAYMENT_SECRET_KEY)
+require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const  port = process.env.PORT || 5000;
 const corsConfig ={origin:'*',
 credentials:true,methods:['GET','POST','PUT','DELETE']}
